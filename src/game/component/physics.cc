67bf2bc45e6b9fe1e28116284e0c0f4fa8eab58a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-05-13 11:05:26 -0300

    Sticky bounds.

--- src/game/component/physics.cc
+++ src/game/component/physics.cc
@@ -16,14 +16,15 @@ Physics::~Physics() {
 }
 
 void Physics::Update(double dt, GameObject* owner) {
-    GameController* controller = owner->game_controller();
+    ugdk::Vector2D map_size = owner->game_controller()->map_size();
+    ugdk::Vector2D next_position = owner->world_position() + owner->velocity() * dt;
 
-    if(owner->world_position().x < 0.0 || owner->world_position().x >= controller->map_size().x)
-        owner->velocity().x *= -1.0;
-    if(owner->world_position().y < 0.0 || owner->world_position().y >= controller->map_size().y)
-        owner->velocity().y *= -1.0;
+    if(next_position.x < 0) next_position.x = 0;
+    if(next_position.y < 0) next_position.y = 0;
+    if(next_position.x > map_size.x) next_position.x = map_size.x;
+    if(next_position.y > map_size.y) next_position.y = map_size.y;
 
-    owner->set_world_position(owner->world_position() + owner->velocity() * dt);
+    owner->set_world_position(next_position);
 }
 
 } // namespace component
